658f527b9adc889859c3825c5f30f8d204030141
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-03-21 15:07:37 +0700

    21-maret-2023

---
+++ 026/konten/galeri.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../style/style.css" />
+  <title>My Galeri</title>
+</head>
+
+<body>
+  <div id="container">
+    <div id="header">
+      <h1>PERTEMUAN I -- SISTEM PAKAR</h1>
+    </div>
+
+    <div id="flex-wrapper">
+      <div id="sidebar">
+        <h3>navigasi</h3>
+        <ul id="navmenu">
+          <li><a href="../index.php" class="selected">Profil</a></li>
+          <li><a href="galeri.php">Galeri</a></li>
+          <li><a href="jadwal.php">Jadwal Kuliah</a></li>
+        </ul>
+      </div>
+
+      <div id="page" class="wrapper-galeri">
+        <div>
+          <p>Ini Halaman Galeri Dengan Gambar Rubik</p>
+          <img src="../images/rubik.jpeg" width="145" height="117">
+        </div>
+
+        <div>
+          <p>Ini Halaman Galeri Dengan Gambar Burung</p>
+          <img src="../images/burung.jpeg" width="145" height="117">
+        </div>
+
+        <div>
+          <p>Ini Halaman Galeri Dengan Gambar Sunset</p>
+          <img src="../images/sungset.jpeg" width="145" height="117">
+        </div>
+      </div>
+    </div>
+
+    <div id="footer">
+      <p>&copy; 2010</p>
+    </div>
+  </div>
+</body>
+
+</html>
